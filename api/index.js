fe5cf46a61e3a1b09e1a45c013e0f40bfcb19010
Himanshu <hs7772043@gmail.com>
2024-02-18 17:43:15 +0530

    edit middleware

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use('/api/auth', authRoutes)
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
     const message = err.message || 'Internal server error'
-    req.status(statusCode).json({
+    res.status(statusCode).json({
         success: false,
         statusCode,
         message,
